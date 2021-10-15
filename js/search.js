96b60bf782e11ce1c6a2274c85da96b93047f0ad
Stephen Doty <stephendoty826@yahoo.com>
2021-10-15 14:09:03 -0500

    Got search bar working to autofill results and send user to itemPage.

---
+++ js/search.js
@@ -0,0 +1,47 @@
+
+// fetch(`https://api.rawg.io/api/games?key=${apiKeyRAWG}&dates=2015-08-01,2021-09-30&platforms=18,1,7&page_size=40`)
+// .then(response => response.json())
+// .then(data=>{
+//     console.log(data)
+// })
+
+// fetch(`https://www.giantbomb.com/api/games/?api_key=${apiKeyGiantBomb}&format=json`)
+// .then(response => response.json())
+// .then(data=>{
+//     console.log(data)
+// })
+
+//https://api.rawg.io/api/games/1454?key=${apiKeyRAWG}
+
+
+// going to need to use the filter method on the games array when user is 
+
+console.log(games_temp) //todo change to games when search bar is working
+let navbar = document.querySelector(".navbar")
+let searchInput = document.querySelector('#searchBar')
+let searchResults = document.querySelector(".searchResults")
+searchInput.addEventListener('keyup', (e) => {
+    let gameFilter = []
+    let displayGameResults = ""
+    if(searchInput.value){
+        gameFilter = games_temp.filter(game => { //todo change to games when search bar is working
+            return game.name.toLowerCase().includes(e.target.value.toLowerCase())
+        })
+    }
+
+    gameFilter.forEach(game => {
+        displayGameResults += `<div id=${game.id} class="displayGameResults">${game.name}</div>`
+    })
+
+    searchResults.innerHTML = displayGameResults
+
+    if(e.code == "Enter"){
+        console.log(gameFilter);
+    }
+
+})
+
+searchResults.addEventListener("click", (e) => {
+    localStorage.id = e.target.id
+    window.location.href = "itemPage.html"
+})
