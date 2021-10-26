9e66712a8214f32d8e4ea2ba934a01d189dd985a
Stephen Doty <stephendoty826@yahoo.com>
2021-10-26 09:03:51 -0500

    removing apiKeys.js from .gitignore so the site will run. Couldn't find out how to use enviromental variables via netlify and Veronica has not had time to look into it.

---
+++ js/apiKeys.js
@@ -0,0 +1,11 @@
+// current key is apiKeyRAWGDevin
+
+let apiKeyRAWG = "c68e19bbd83d409b9b7fefea1d344ed0"
+
+let apiKeyRAWGStephen = "d56bf38c7d1e419583f481f1b5ef2bcc"
+
+let apiKeyRAWGDevin = "c68e19bbd83d409b9b7fefea1d344ed0"
+
+let apiKeyRAWGJames = "6981ca6d11994d9abc7a70b4bed8e64b"
+
+let apiKeyGiantBomb = "34e5458134aae96be4669cef6451c79d20c53a8f"
